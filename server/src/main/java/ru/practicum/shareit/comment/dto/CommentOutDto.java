13510ad1df0490a6f9ff74055c98369d359e8b77
Stanislav Makarov <stanmakarov@yandex.ru>
2023-06-28 14:32:45 +0300

    FIT: ТЗ 16

--- server/src/main/java/ru/practicum/shareit/comment/dto/CommentOutDto.java
+++ server/src/main/java/ru/practicum/shareit/comment/dto/CommentOutDto.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.comment.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
--- server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestOutDto.java
+++ server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestOutDto.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.request.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.practicum.shareit.item.dto.ItemOutDto;
